e225478b6fd2cb933c8f1de6c769cd7c7345e2f0
pranav2781 <potdarpranav290@gmail.com>
2021-05-02 23:45:51 +0530

    Restored python file

---
+++ NewFriend.py
@@ -0,0 +1,17 @@
+name = input("\n \nWhat is your name? \n")
+print("Hi " + name +"!") 
+covid = input("\n \nDo you have covid or not?")
+ 
+if covid == "Yes":
+    print("Ohh!..Get well soon")
+    print("1] Drink more warm water stay hydreted")
+    print("2] Take partially stem.it will help you.. ")
+    print("3] Take balenced meal")
+
+elif covid == "No":
+    print("Ohh that's great!!!")
+    print("Take care!!! ")
+
+elif covid == "What":
+    print("_________Are you STUPID?_____________")
+print ("Thank you for your kind support") 
